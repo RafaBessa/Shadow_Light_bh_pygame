6dc14432bdeefe22e7b459ed1699479a8e872600
Rafael Bessa Loureiro <rafablou@hotmail.com>
2020-08-15 13:49:14 -0300

    separação codigo em classes

---
+++ Entity.py
@@ -0,0 +1,33 @@
+
+import pygame
+import os
+
+IMG_ASSETS = {"guy": pygame.image.load(os.path.join("assets", "guy.png")),
+              "droplet": pygame.image.load(os.path.join("assets", "droplet.png"))}
+SCALE_ASSETS = {"guy": .5,
+                "droplet": 5}
+
+class Entity:
+    def __init__(self, key, coordinates, dimensions):
+        self.coordinates = coordinates
+        self._dimensions = dimensions
+
+        self.img_key = key
+        self.img = IMG_ASSETS[key]
+
+    def resize(self, window):
+        self.img, self.coordinates = window.resizedAsset(self.img_key, self.coordinates, self._dimensions)
+        self._dimensions = window.shape
+        self.draw(window)
+
+    def draw(self, window):
+        window.draw(self.img, self.coordinates)
+
+    @property
+    def width(self):
+        return self.img.get_width()
+
+    @property
+    def height(self):
+        return self.img.get_height()
+
---
+++ Player.py
@@ -0,0 +1,51 @@
+
+import Entity
+import pygame
+
+class Player(Entity.Entity):
+
+    def __init__(self, key, coordinates, dimensions, speed):
+        super().__init__(key, coordinates, dimensions)
+        self._speed = speed  # in widths per second
+        self.speed = speed*self.img.get_width()
+
+    def resize(self, window):
+        super().resize(window)
+        self.speed = self._speed * self.img.get_width()
+
+    def walk(self, key, window, dt):
+        u = (key[pygame.K_d] - key[pygame.K_a])
+        v = (key[pygame.K_s] - key[pygame.K_w])
+        modulus = (u ** 2 + v ** 2) ** .5
+
+        # Checks for simultaneous WS or AD presses
+        if modulus > 0:
+            # making linear velocity equal to player velocity
+            modulus = self.speed * dt / modulus
+            u = round(u * modulus)
+            v = round(v * modulus)
+            # Checking left border
+            if self.coordinates[0] + u > 0:
+                # Checking right border
+                if self.coordinates[0] + u < window.width - self.width:
+                    # Moving
+                    self.coordinates[0] += u
+                else:
+                    # Hugging right
+                    self.coordinates[0] = window.width - self.width
+            else:
+                # Hugging left
+                self.coordinates[0] = 0
+
+            # Checking upper border
+            if self.coordinates[1] + v > 0:
+                # Checking lower border
+                if self.coordinates[1] + v < window.height - self.height:
+                    # Moving
+                    self.coordinates[1] += v
+                else:
+                    # Hugging lower border
+                    self.coordinates[1] = window.height - self.height
+            else:
+                # Hugging upper border
+                self.coordinates[1] = 0
